17ce276ea1e7d134f8a3c29ad5e6745250123513
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-02-23 14:19:11 +0700

    adding home page

---
+++ src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
@@ -0,0 +1,14 @@
+package com.bookStore.bookStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookStoreController {
+    
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
