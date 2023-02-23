da2e969e1eb0624523ab44457104bdf2495e5f11
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-02-23 15:04:42 +0700

    adding other pages

--- src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
@@ -11,4 +11,18 @@ public class BookStoreController {
     public String home(){
         return "home";
     }
+
+    @GetMapping("/add_book")
+    public String addBook(){
+        return "addBook";
+    }
+
+    @GetMapping("/library")
+    public String library(){
+        return "library";
+    }
+    @GetMapping("/my_book")
+    public String myBook(){
+        return "myBook";
+    }
 }
