bed1ea59c9bc88d7f940197b40dd0a909bbf519a
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-02-25 20:22:26 +0700

    add operation save book

--- src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
@@ -1,11 +1,19 @@
 package com.bookStore.bookStore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bookStore.bookStore.entity.Book;
+import com.bookStore.bookStore.services.BookService;
 
 @Controller
 public class BookStoreController {
     
+    @Autowired
+    private BookService service;
 
     @GetMapping("/")
     public String home(){
@@ -21,8 +29,15 @@ public class BookStoreController {
     public String library(){
         return "library";
     }
+
     @GetMapping("/my_book")
     public String myBook(){
         return "myBook";
+    }
+
+    @PostMapping("/save")
+    public String addBook(@ModelAttribute Book b){
+        service.save(b);
+        return "redirect:/library";
     }
 }
