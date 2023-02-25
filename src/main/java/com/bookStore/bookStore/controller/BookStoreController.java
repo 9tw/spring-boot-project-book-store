39b7c0265787a6997305c525c60e2d9e8b1bcce8
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-02-25 20:40:34 +0700

    add operation show all books

--- src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
@@ -1,10 +1,13 @@
 package com.bookStore.bookStore.controller;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bookStore.bookStore.entity.Book;
 import com.bookStore.bookStore.services.BookService;
@@ -26,8 +29,12 @@ public class BookStoreController {
     }
 
     @GetMapping("/library")
-    public String library(){
-        return "library";
+    public ModelAndView library(){
+        List<Book> list=service.getAllBook();
+        ModelAndView m = new ModelAndView();
+        m.setViewName("library");
+        m.addObject("book", list);
+        return new ModelAndView("library", "book", list);
     }
 
     @GetMapping("/my_book")
--- src/main/java/com/bookStore/bookStore/services/BookService.java
+++ src/main/java/com/bookStore/bookStore/services/BookService.java
@@ -1,5 +1,7 @@
 package com.bookStore.bookStore.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class BookService {
 
     public void save(Book b){
         bRepo.save(b);
+    }
+
+    public List<Book> getAllBook(){
+        return bRepo.findAll();
     }
 }
