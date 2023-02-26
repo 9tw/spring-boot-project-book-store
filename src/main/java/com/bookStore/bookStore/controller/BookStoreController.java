082490aa7dbdb7f181b96e2d764e3aa033753b6b
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-02-26 22:02:00 +0700

    add borrow book page and operation

--- src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookStoreController.java
@@ -4,19 +4,27 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bookStore.bookStore.entity.Book;
+import com.bookStore.bookStore.entity.MyBookList;
 import com.bookStore.bookStore.services.BookService;
+import com.bookStore.bookStore.services.MyBookListService;
 
 @Controller
 public class BookStoreController {
     
     @Autowired
     private BookService service;
+
+    @Autowired
+    private MyBookListService myBookService;
 
     @GetMapping("/")
     public String home(){
@@ -38,7 +46,9 @@ public class BookStoreController {
     }
 
     @GetMapping("/my_book")
-    public String myBook(){
+    public String myBook(Model model){
+        List<MyBookList> list = myBookService.getAllBooks();
+        model.addAttribute("book", list);
         return "myBook";
     }
 
@@ -46,5 +56,13 @@ public class BookStoreController {
     public String addBook(@ModelAttribute Book b){
         service.save(b);
         return "redirect:/library";
+    }
+
+    @RequestMapping("/mylist/{id}")
+    public String getMyList(@PathVariable("id") int id){
+        Book b = service.getBookById(id);
+        MyBookList mb = new MyBookList(b.getId(), b.getName(), b.getAuthor(), b.getPrice());
+        myBookService.saveMyBooks(mb);
+        return "redirect:/my_book";
     }
 }
